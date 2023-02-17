5ca8e5f9f2bfda3c73b851be777eba948823368a
lelinrashed <lelin.rashed784@gmail.com>
2023-02-17 22:06:04 +0600

    lecture 2

---
+++ variable/Main.java
@@ -0,0 +1,25 @@
+package variable;
+
+public class Main {
+    public static void main(String[] args) {
+        /**
+         * java lang all data type Primitive
+         * byte, short, int, long, float, double, char, boolean
+         */
+
+        // int a = 10;
+        // int b = 20;
+        // int sum = a + b;
+        // System.out.println(sum);
+
+        /**
+         * Java lang non primitive data type
+         * String, Array, Class, Object, Interface
+         */
+
+        // input system in java
+        // Scanner sc = new Scanner(System.in);
+        // String name = sc.nextLine();
+        // System.out.println(name);
+    }
+}

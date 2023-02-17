c2ff64b0a81dfdb4a738f43cd08daa7eb79a2562
lelinrashed <lelin.rashed784@gmail.com>
2023-02-17 23:52:42 +0600

    some problem solve about nested loop

---
+++ loops/Main.java
@@ -0,0 +1,24 @@
+package loops;
+
+public class Main {
+    public static void main(String[] args) {
+        // java all loops example
+        // for loop
+        // for (initialization; condition; increment/decrement) {
+        //     // code
+        // }
+        // for (int i = 0; i < 10; i++) {
+        //     System.out.println(i);
+        // }
+        // for (int i = 10; i > 0; i--) {
+        //     System.out.println(i);
+        // }
+        // for (int i = 0; i < 10; i += 2) {
+        //     System.out.println(i);
+        // }
+        // for (int i = 10; i > 0; i -= 2) {
+        //     System.out.println(i);
+        // }
+
+    }
+}

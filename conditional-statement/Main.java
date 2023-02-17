b8569f42d9694a21878600ea9262afaf31294b41
lelinrashed <lelin.rashed784@gmail.com>
2023-02-17 22:14:16 +0600

    lecture 3 conditional statement

---
+++ conditional-statement/Main.java
@@ -0,0 +1,90 @@
+public class Main {
+    public static void main(String[] args) {
+        // if statement
+        // if (condition) {
+        //     // code
+        // }
+
+        // if (condition) {
+        //     // code
+        // } else {
+        //     // code
+        // }
+
+        // if (condition) {
+        //     // code
+        // } else if (condition) {
+        //     // code
+        // } else {
+        //     // code
+        // }
+
+        // switch (expression) {
+        //     case value:
+        //         // code
+        //         break;
+        //     case value:
+        //         // code
+        //         break;
+        //     default:
+        //         // code
+        //         break;
+        // }
+
+        // ternary operator
+        // condition ? true : false;
+        // int a = 10;
+        // int b = 20;
+        // int max = a > b ? a : b;
+        // System.out.println(max);
+
+        // int a = 10;
+        // int b = 20;
+        // int max;
+        // if (a > b) {
+        //     max = a;
+        // } else {
+        //     max = b;
+        // }
+        // System.out.println(max);
+
+        // int a = 10;
+        // int b = 20;
+        // int max;
+        // switch (a > b) {
+        //     case true:
+        //         max = a;
+        //         break;
+        //     case false:
+        //         max = b;
+        //         break;
+        //     default:
+        //         break;
+        // }
+        // System.out.println(max);
+
+        // int a = 10;
+        // int b = 20;
+        // int max;
+        // if (a > b) {
+        //     max = a;
+        // } else if (a < b) {
+        //     max = b;
+        // } else {
+        //     max = a;
+        // }
+        // System.out.println(max);
+
+        // int a = 10;
+        // int b = 20;
+        // int max;
+        // switch (a > b) {
+        //     case true:
+        //         max = a;
+        //         break;
+        //     case false:
+        //         switch (a < b) {
+        //             case true:
+        //                 max = b;
+    }
+}

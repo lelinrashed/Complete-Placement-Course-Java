f44801e823eb958672b15e14f88b3caa6b84b15b
lelinrashed <lelin.rashed784@gmail.com>
2023-02-21 14:10:22 +0600

    string

--- arrays/Arrays.java
+++ arrays/Arrays.java
@@ -4,19 +4,20 @@ import java.util.Scanner;
 
 public class Arrays {
     public static void main(String[] args) {
-        // Scanner
-        Scanner sc = new Scanner(System.in);
-        int size = sc.nextInt();
-        int numbers[] = new int[size];
+        try (// Scanner
+        Scanner sc = new Scanner(System.in)) {
+            int size = sc.nextInt();
+            int numbers[] = new int[size];
 
-        // Input
-        for (int i = 0; i < size; i++) {
-            numbers[i] = sc.nextInt();
-        }
+            // Input
+            for (int i = 0; i < size; i++) {
+                numbers[i] = sc.nextInt();
+            }
 
-        // Output
-        for (int i = 0; i < size; i++) {
-            System.out.println(numbers[i]);
+            // Output
+            for (int i = 0; i < size; i++) {
+                System.out.println(numbers[i]);
+            }
         }
 
     }
---
+++ string/Strings.java
@@ -0,0 +1,24 @@
+package string;
+
+public class Strings {
+    public static void main(String[] args) {
+        // Hello
+        
+
+
+        StringBuilder sb = new StringBuilder("Hello");
+
+        for (int i = 0; i < sb.length() / 2; i++) {
+            int front = i;
+            int back = sb.length() - 1 - i;
+
+            char frontChar = sb.charAt(front);
+            char backChar = sb.charAt(back);
+
+            sb.setCharAt(front, backChar);
+            sb.setCharAt(back, frontChar);
+        }
+
+        System.out.println(sb);
+    }
+}
